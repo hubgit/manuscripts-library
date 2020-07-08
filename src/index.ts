b90710f2d1fb67d6b5ef0ca5c206a4a39c50d1ae
Alf Eaton <aeaton@atypon.com>
2020-07-08 22:24:46 +0100

    Remove csl-json import

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// eslint-disable-next-line import/no-unresolved
-import 'csl-json'
 
 export * from './lib'
